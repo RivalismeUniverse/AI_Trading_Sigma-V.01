54ab5c51716cb032c092c0fe314286e9f0f08c47
RivalismeUniverse <aceharival@gmail.com>
2025-12-18 12:19:02 +0700

    Update __init__.py

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,3 +1,14 @@
-"""
-Core trading engine module
-"""
+"""Core trading engine components"""
+from .hybrid_engine import HybridTradingEngine
+from .signal_generator_v1 import SignalGeneratorV1
+from .signal_validator_v2 import SignalValidatorV2
+from .integrated_signal_manager import IntegratedSignalManager
+from .risk_manager import RiskManager
+
+__all__ = [
+    'HybridTradingEngine',
+    'SignalGeneratorV1',
+    'SignalValidatorV2',
+    'IntegratedSignalManager',
+    'RiskManager'
+]
