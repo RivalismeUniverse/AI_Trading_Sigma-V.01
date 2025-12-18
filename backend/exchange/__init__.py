0da5ed136e56de24bfeb868bddeb22d3ff71a76b
RivalismeUniverse <aceharival@gmail.com>
2025-12-18 12:20:02 +0700

    Update __init__.py

--- backend/exchange/__init__.py
+++ backend/exchange/__init__.py
@@ -1,3 +1,13 @@
-"""
-Exchange integration module
-"""
+"""Exchange client implementations"""
+from .base_client import BaseExchangeClient
+from .weex_client import WEEXClient
+from .binance_client import BinanceClient
+from .safety_checker import SafetyChecker, get_safety_checker
+
+__all__ = [
+    'BaseExchangeClient',
+    'WEEXClient',
+    'BinanceClient',
+    'SafetyChecker',
+    'get_safety_checker'
+]
